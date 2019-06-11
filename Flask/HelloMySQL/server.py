d6bdde334738f17babcc3c02654b6ffdf82ee5bd
Devon <dev206@gmail.com>
2019-06-11 11:11:08 -0700

    adding validations to homies

--- Flask/HelloMySQL/server.py
+++ Flask/HelloMySQL/server.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session
 from mysqlconnection import connectToMySQL
+import re
+
+# re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 app = Flask(__name__)
+app.secret_key = "sadf3jwckjlksdjfl"
 
 @app.route("/")
 def index():
+
+    for key in session:
+        print(session[key])
+
     mysql = connectToMySQL("mydb")
     homies = mysql.query_db("SELECT * FROM homies")
     print(homies)
@@ -16,13 +25,27 @@ def create():
 
     data = {
         "name": request.form["name"],
+        "em": request.form["email"],
         "loc": request.form["location"]
     }
-
-    insert = "INSERT INTO homies (name, location) VALUES (%(name)s, %(loc)s);"
+    # we are invalid if there is a _flashes key in session
+    # check data for all fields containging data
     
-    mysql = connectToMySQL("mydb")
-    mysql.query_db(insert, data)
+    if len(request.form["name"]) < 1:
+        flash("Name field is required")
+    if len(request.form["email"]) < 1:
+        flash("Email field is required")
+    if len(request.form["location"]) < 1:   
+        flash("Location field is required")
+    if not EMAIL_REGEX.match(request.form["email"]):
+        flash("Invalid Email address")
+    
+
+    if not "_flashes" in session.keys():
+        insert = "INSERT INTO homies (name, email, location) VALUES (%(name)s, %(em)s, %(loc)s);"
+        
+        mysql = connectToMySQL("mydb")
+        mysql.query_db(insert, data)
     return redirect("/")
 
 @app.route("/show/<user_id>")
